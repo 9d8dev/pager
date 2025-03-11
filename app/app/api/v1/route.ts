070a7522254edb69e0bb85a99fb8f3bad7806ae8
youngbloodcyb <cameron.youngblood@gmail.com>
2025-03-11 15:04:03 -0700

    update

--- app/app/api/v1/route.ts
+++ app/app/api/v1/route.ts
@@ -1,3 +1,18 @@
+import { headers } from "next/headers";
+
 export async function POST(request: Request) {
+  const headersList = await headers();
+  const authorization = headersList.get("authorization");
+
+  if (!authorization) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  // await upstash(); --> send to upstash
+  //  api to slack
+  //  api to discord
+  //  api to webhook
+  //  api to email
+
   return Response.json({ message: "pager.dev v1 api" });
 }
