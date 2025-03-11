844f68e0fcdbd3ec6a2cc8f1dc78eb9d22a03f90
youngbloodcyb <cameron.youngblood@gmail.com>
2025-03-10 21:50:49 -0700

    [auth] add toast

--- app/components/auth/signout.tsx
+++ app/components/auth/signout.tsx
@@ -3,9 +3,17 @@
 import { Button } from "@/components/ui/button";
 import { signOut } from "@/lib/auth/actions";
 import { useAction } from "next-safe-action/hooks";
+import { toast } from "sonner";
 
 export function SignOut() {
-  const { execute, isExecuting } = useAction(signOut);
+  const { execute, isExecuting } = useAction(signOut, {
+    onSuccess() {
+      toast.success("Signed out successfully.");
+    },
+    onError() {
+      toast.error("Failed to sign out.");
+    },
+  });
   return (
     <Button disabled={isExecuting} onClick={() => execute()}>
       Sign Out
