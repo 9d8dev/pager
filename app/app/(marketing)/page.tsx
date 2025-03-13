a5d0ad45e5ef6ddde61475408813684918d3a771
Bridger Tower <bridgertower@gmail.com>
2025-03-12 21:58:40 -0600

    cookin

--- app/app/(marketing)/page.tsx
+++ app/app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+import Pager from "@/public/pager.webp";
 import Logo2 from "@/public/logo-2.svg";
 import Logo from "@/public/logo.svg";
 import Image from "next/image";
@@ -7,9 +8,9 @@ import { ThemeToggle } from "@/components/theme/theme-toggle";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex items-center justify-center">
+    <main className="w-screen h-screen relative">
       <section className="space-y-12 text-center uppercase font-mono">
-        <div>
+        <div className="absolute top-6 left-6">
           <h1 className="sr-only">Pager</h1>
           <Image src={Logo} alt="Logo" width={180} className="dark:hidden" />
           <Image
@@ -20,13 +21,19 @@ export default function Home() {
           />
         </div>
 
-        <div className="flex gap-6 items-center justify-center">
+        <div className="flex gap-6 items-center justify-center absolute bottom-6 left-6">
           <Link href="/dashboard" className="hover:font-semibold">
             go to app
           </Link>
           <ThemeToggle />
         </div>
       </section>
+
+      <Image
+        src={Pager}
+        alt="Pager"
+        className="object-cover h-screen absolute dark:invert -z-10"
+      />
     </main>
   );
 }
