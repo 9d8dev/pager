1ead2c2ccebf2217f5ded2617916a9c0f84a3671
Bridger Tower <bridgertower@gmail.com>
2025-03-12 21:50:56 -0600

    wip

--- app/app/(marketing)/page.tsx
+++ app/app/(marketing)/page.tsx
@@ -3,23 +3,29 @@ import Logo from "@/public/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 
+import { ThemeToggle } from "@/components/theme/theme-toggle";
+
 export default function Home() {
   return (
     <main className="w-screen h-screen flex items-center justify-center">
       <section className="space-y-12 text-center uppercase font-mono">
-        <h1 className="sr-only">Pager</h1>
+        <div>
+          <h1 className="sr-only">Pager</h1>
+          <Image src={Logo} alt="Logo" width={180} className="dark:hidden" />
+          <Image
+            src={Logo2}
+            alt="Logo"
+            width={180}
+            className="hidden dark:block"
+          />
+        </div>
 
-        <Image src={Logo} alt="Logo" width={180} className="dark:hidden" />
-        <Image
-          src={Logo2}
-          alt="Logo"
-          width={180}
-          className="hidden dark:block"
-        />
-
-        <Link href="/dashboard" className="hover:font-semibold">
-          go to app
-        </Link>
+        <div className="flex gap-6 items-center justify-center">
+          <Link href="/dashboard" className="hover:font-semibold">
+            go to app
+          </Link>
+          <ThemeToggle />
+        </div>
       </section>
     </main>
   );
--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Instrument_Sans, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const instrumentSans = Instrument_Sans({
   variable: "--font-instrument-sans",
