b4680e3ed3e378edcad1503f1bfc03a5b3dcaca0
Bridger Tower <bridgertower@gmail.com>
2025-03-12 21:29:46 -0600

    wip

--- app/app/(marketing)/page.tsx
+++ app/app/(marketing)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="p-6 font-[family-name:var(--font-geist-sans)]">
-      <section className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="w-screen h-screen bg-primary flex items-center justify-center">
+      <section>
         <h1 className="text-2xl font-medium font-mono">Welcome to Pager</h1>
         <p>this is the marketing page</p>
         <Link href="/dashboard" className="underline">
